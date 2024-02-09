67cac1d96f8c1759b3534e65ac5047dae4ffd716
omercan <ocansever493@gmail.com>
2024-02-09 13:22:47 +0300

    Add project files.

---
+++ Business/Concrete/PttManager.cs
@@ -0,0 +1,29 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class PttManager : ISupplierService
+    {
+        IApplicantService _applicantService;
+        public PttManager(IApplicantService applicantService)
+        {
+            _applicantService = applicantService; //Constructor New Edildiğinde Çalışır
+        }
+        public void GiveMask(Person person)
+        {
+            if (_applicantService.CheckPerson(person))
+            {
+                Console.WriteLine(person.FirstName + " İçin Maske Verildi");
+            }else
+            {
+                Console.WriteLine(person.FirstName + " İçin Maske VERİLEMEDİ");
+            }
+        }
+    }
+}
---
+++ Workaround/Program.cs
@@ -0,0 +1,20 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+
+namespace Workaround;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Person person1 = new Person();
+        person1.FirstName = "";
+        person1.LastName = "";
+        person1.NationalIdentity = 123;
+        person1.DateOfBirthYear = 123;
+
+
+        PttManager pttManager = new PttManager(new PersonManager());
+        pttManager.GiveMask(person1);
+    }
+}
